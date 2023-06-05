cb56ad45e67e916a5e989fbce500e8789191b7a0
Kanra Su <karawakara@gmail.com>
2023-06-04 17:36:54 -0700

    v1.3.3

--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -5,6 +5,8 @@ import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import styles from './index.module.css';
+import packageJson from "../../../package.json";
+
 
 function HomepageHeader() {
     const { siteConfig } = useDocusaurusContext();
@@ -12,16 +14,16 @@ function HomepageHeader() {
         <header className={clsx('', styles.heroBanner)}>
             <div className={styles.heroBannerBg}></div>
             <div className="container" style={{ position: "relative" }}>
-                <h1 style={{ fontSize: "2.75rem" }}>{siteConfig.title}</h1>
-                <h3 >
+                <h1 style={{ fontSize: "2.5rem" }}>{siteConfig.title}</h1>
+                <h4 >
                     A simple slider using css style <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Scroll_Snap" target="_blank">scroll-snap</a> and <a className="" style={{ color: "#FA196C", fontStyle: "italic" }} href="https://popmotion.io/" target="_blank">Popmotion</a>
-                </h3>
+                </h4>
                 <div className={styles.buttons}>
-                    {/* <Link
-                        className="button button--secondary button--lg"
+                    <Link
+                        className="button button--primary button--lg"
                         to="/docs/intro">
-                        Docusaurus Tutorial - 5minfffffff ⏱️
-                    </Link> */}
+                        Get Started <b style={{ fontSize: "12px" }}>(v{packageJson.version})</b>
+                    </Link>
                 </div>
             </div>
         </header>
